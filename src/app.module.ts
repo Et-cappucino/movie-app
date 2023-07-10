ca7f36f17b167a5c201ad22b4dc3b1b317c24eb5
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-07-11 00:45:24 +0400

    Add and update raw User layers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './db/database.module';
 import { ProfileModule } from './profile/profile.module';
+import { UserModule } from './user/user.module';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), 
     ProfileModule, 
-    DatabaseModule,
+    DatabaseModule, 
+    UserModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/user/user.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto, UpdateUserDto } from './dto';
+
+@Controller('api/users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  signUp(@Body() createUserDto: CreateUserDto) {
+    return this.userService.signUp(createUserDto);
+  }
+
+  @Get()
+  findAllUsers() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findUser(@Param('id') id: number) {
+    return this.userService.findOne(id);
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  removeUser(@Param('id') id: number) {
+    return this.userService.remove(id);
+  }
+}
