88bb525e868ae9ab771fd0bf5ed3b352550cf2cd
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-07-19 20:27:32 +0400

    Add Swagger package, update Main with Swagger configs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,15 @@ async function bootstrap() {
     transform: true,
     whitelist: true,
   }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Movie API')
+    .setDescription('Movie API documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/swagger', app, document);
+
   await app.listen(3333);
 }
 bootstrap();
