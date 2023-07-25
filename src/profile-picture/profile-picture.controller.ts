bb4585f52ef211dc389717fbddcec28730f29973
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-07-25 23:43:20 +0400

    Update Profile Picture controller with upload validations for file type and size

--- src/profile-picture/profile-picture.controller.ts
+++ src/profile-picture/profile-picture.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Param, Delete, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post, Param, Delete, UploadedFile, UseInterceptors, ParseFilePipeBuilder, HttpStatus } from '@nestjs/common';
 import { Express } from 'express';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ProfilePictureService } from './profile-picture.service';
@@ -9,7 +9,12 @@ export class ProfilePictureController {
 
   @Post('upload/:profileId')
   @UseInterceptors(FileInterceptor('file'))
-  uploadProfilePicture(@UploadedFile() imageFile: Express.Multer.File, @Param('profileId') profileId: number) {
+  uploadProfilePicture(@UploadedFile(
+    new ParseFilePipeBuilder()
+    .addFileTypeValidator({ fileType: /(jpg|jpeg|png)$/ })
+    .addMaxSizeValidator({ maxSize: 1000000 })
+    .build({ errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY })
+  ) imageFile: Express.Multer.File, @Param('profileId') profileId: number) {
     this.profilePictureService.uploadProfilePicture(imageFile, profileId);
   }
 
