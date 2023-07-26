26b938d784efd93ab4681c501f4a906b3bdf1506
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-07-27 00:43:23 +0400

    Update Actor entity and dto with fields and validations

--- src/actor/dto/create-actor.dto.ts
+++ src/actor/dto/create-actor.dto.ts
@@ -1,1 +1,23 @@
-export class CreateActorDto {}
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreateActorDto {
+
+    @IsString()
+    @IsNotEmpty()
+    firstName: string;
+
+    @IsString()
+    @IsNotEmpty()
+    lastName: string;
+
+    @IsOptional()
+    birthDate: Date;
+
+    @IsString()
+    @IsOptional()
+    bio: string;
+
+    @IsString()
+    @IsOptional()
+    imagePath: string;
+}
--- src/actor/entities/actor.entity.ts
+++ src/actor/entities/actor.entity.ts
@@ -1,1 +1,23 @@
-export class Actor {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Actor {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ name: 'first_name' })
+    firstName: string;
+
+    @Column({ name: 'last_name' })
+    lastName: string;
+
+    @Column({ name: 'birth_date', type: 'date', nullable: true })
+    birthDate: Date;
+
+    @Column({ name: 'bio', type: 'text', nullable: true })
+    bio: string;
+
+    @Column({ name: 'image_path', nullable: true })
+    imagePath: string;
+}
