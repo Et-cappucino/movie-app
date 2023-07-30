b29699be4f2dc4f6f0bb2adcc6cef6a9f85fa40c
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-07-30 21:01:34 +0400

    Update Rate entity and dto with fields and validations

--- src/rate/dto/create-rate.dto.ts
+++ src/rate/dto/create-rate.dto.ts
@@ -1,1 +1,25 @@
-export class CreateRateDto {}
+import { IsNotEmpty, IsNumber, IsPositive, Max, Min } from "class-validator";
+
+export class CreateRateDto {
+
+    @Min(0)
+    @Max(5)
+    @IsNumber()
+    @IsNotEmpty()
+    @IsPositive()
+    questionOneValue: number;
+
+    @Min(0)
+    @Max(5)
+    @IsNumber()
+    @IsNotEmpty()
+    @IsPositive()
+    questionTwoValue: number;
+
+    @Min(0)
+    @Max(5)
+    @IsNumber()
+    @IsNotEmpty()
+    @IsPositive()
+    questionThreeValue: number;
+}
--- src/rate/entities/rate.entity.ts
+++ src/rate/entities/rate.entity.ts
@@ -1,1 +1,20 @@
-export class Rate {}
+import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn } from "typeorm";
+
+@Entity()
+export class Rate {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ name: 'question_one_value', type: 'double' })
+    questionOneValue: number;
+
+    @Column({ name: 'question_two_value', type: 'double' })
+    questionTwoValue: number;
+
+    @Column({ name: 'question_three_value', type: 'double' })
+    questionThreeValue: number;
+
+    @CreateDateColumn({ name: 'rated_at' })
+    ratedAt: Date;
+}
