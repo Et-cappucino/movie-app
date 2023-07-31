2d844cfdf15c1794092d237ed328c90199334e2c
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-07-31 22:42:23 +0400

    Update Search entity and controller with documentation

--- src/search/entities/search-record.entity.ts
+++ src/search/entities/search-record.entity.ts
@@ -1,18 +1,23 @@
 import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { ApiProperty } from "@nestjs/swagger";
 import { Profile } from "src/profile/entities/profile.entity";
 
 @Entity()
 export class SearchRecord {
 
+    @ApiProperty()
     @PrimaryGeneratedColumn({ type: 'bigint' })
     id: number;
 
+    @ApiProperty()
     @CreateDateColumn({ name: 'created_at'})
     createdAt: Date;
 
+    @ApiProperty()
     @Column({ name: 'searched_text' })
     text: string;
 
+    @ApiProperty({ type: () => Profile })
     @ManyToOne(() => Profile, (profile) => profile.searches, { onDelete: 'CASCADE' })
     @JoinColumn({ name: 'profile_id' })
     profile: Profile;
--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,11 +1,21 @@
 import { Controller, Get, Query } from '@nestjs/common';
+import { ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { SearchService } from './search.service';
 import { GenreEnum, WatchableType } from 'src/watchable/enums';
+import { Watchable } from 'src/watchable/entities';
 
+@ApiTags('Search-Controller')
 @Controller('api/search')
 export class SearchController {
   constructor(private readonly searchService: SearchService) {}
 
+  @ApiQuery({ name: 'pageNumber', example: 0 })
+  @ApiQuery({ name: 'pageSize', example: 5 })
+  @ApiOkResponse({ 
+    type: Watchable, 
+    isArray: true,
+    description: 'Retrieve Watchables by search with pagination and sorting support.' 
+  })
   @Get()
   searchWatchables(@Query('query') query: string,
                    @Query('pageNumber') pageNumber: number = 0, 
@@ -13,6 +23,13 @@ export class SearchController {
     return this.searchService.searchWatchables(query, pageNumber, pageSize);
   }
 
+  @ApiQuery({ name: 'pageNumber', example: 0 })
+  @ApiQuery({ name: 'pageSize', example: 5 })
+  @ApiOkResponse({ 
+    type: Watchable, 
+    isArray: true,
+    description: 'Retrieve Watchables by search with pagination and sorting support when Logged In.' 
+  })
   @Get('userSearch')
   userSearchWatchables(@Query('query') query: string,
                        @Query('email') email: string,
@@ -21,6 +38,15 @@ export class SearchController {
     return this.searchService.searchWatchables(query, pageNumber, pageSize, email);
   }
 
+  @ApiQuery({ name: 'pageNumber', example: 0 })
+  @ApiQuery({ name: 'pageSize', example: 5 })
+  @ApiQuery({ name: 'genre', enum: GenreEnum })
+  @ApiQuery({ name: 'type', enum: WatchableType, required: false })
+  @ApiOkResponse({ 
+    type: Watchable, 
+    isArray: true,
+    description: 'Retrieve Watchables by Genre and Type search with pagination and sorting support.' 
+  })
   @Get('type-genre')
   searchWatchablesByTypeAndGenre(@Query('genre') genre: GenreEnum,
                                  @Query('type') type: WatchableType,
@@ -29,6 +55,14 @@ export class SearchController {
     return this.searchService.findByTypeAndGenre(genre, pageNumber, pageSize, type);
   }
 
+  @ApiQuery({ name: 'pageNumber', example: 0 })
+  @ApiQuery({ name: 'pageSize', example: 5 })
+  @ApiQuery({ name: 'type', enum: WatchableType, required: false })
+  @ApiOkResponse({ 
+    type: Watchable, 
+    isArray: true,
+    description: 'Retrieve Watchables by ReleaseYear and Type search with pagination and sorting support.' 
+  })
   @Get('type-releaseYear')
   searchWatchablesByTypeAndReleaseYear(@Query('year') year: number,
                                        @Query('type') type: WatchableType,
