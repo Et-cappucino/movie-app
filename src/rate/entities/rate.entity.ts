79d8645db8b352843490fa0b8c39cc8934d6cbc6
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-07-31 11:31:48 +0400

    Update Rate entity, dto, and controller with documentation

--- src/rate/entities/rate.entity.ts
+++ src/rate/entities/rate.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Profile } from "src/profile/entities/profile.entity";
 import { Watchable } from "src/watchable/entities";
 import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, ManyToOne, JoinColumn } from "typeorm";
@@ -5,25 +6,32 @@ import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, ManyToOne, Jo
 @Entity()
 export class Rate {
 
+    @ApiProperty()
     @PrimaryGeneratedColumn()
     id: number;
 
+    @ApiProperty()
     @Column({ name: 'question_one_value', type: 'double' })
     questionOneValue: number;
 
+    @ApiProperty()
     @Column({ name: 'question_two_value', type: 'double' })
     questionTwoValue: number;
 
+    @ApiProperty()
     @Column({ name: 'question_three_value', type: 'double' })
     questionThreeValue: number;
 
+    @ApiProperty()
     @CreateDateColumn({ name: 'rated_at' })
     ratedAt: Date;
 
+    @ApiProperty({ type: () => Profile })
     @ManyToOne(() => Profile, (profile) => profile.rates, { onDelete: 'CASCADE' })
     @JoinColumn({ name: 'profile_id' })
     profile: Profile;
 
+    @ApiProperty({ type: () => Watchable })
     @ManyToOne(() => Watchable, (watchable) => watchable.rates, { onDelete: 'CASCADE' })
     @JoinColumn({ name: 'watchable_id' })
     watchable: Watchable;
