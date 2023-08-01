6499b922003116d8111530174fc2546eba941689
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-08-01 22:06:17 +0400

    Update Filter controller with documentation

--- src/filter/filter.controller.ts
+++ src/filter/filter.controller.ts
@@ -1,11 +1,22 @@
 import { Controller, Get, Query } from '@nestjs/common';
+import { ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { FilterService } from './filter.service';
 import { WatchableType } from 'src/watchable/enums';
+import { Watchable } from 'src/watchable/entities';
 
+@ApiTags('Filter-Controller')
 @Controller('api/filter')
 export class FilterController {
   constructor(private readonly filterService: FilterService) {}
 
+  @ApiQuery({ name: 'pageNumber', example: 0 })
+  @ApiQuery({ name: 'pageSize', example: 10 })
+  @ApiQuery({ name: 'type', enum: WatchableType })
+  @ApiOkResponse({ 
+    type: Watchable, 
+    isArray: true,
+    description: 'Retrieve all Watchables by Type with pagination and sorting support.' 
+  })
   @Get()
   findAllWatchablesByType(@Query('pageNumber') pageNumber: number = 0, 
                           @Query('pageSize') pageSize: number = 10,
@@ -13,6 +24,14 @@ export class FilterController {
     return this.filterService.findAllWatchablesByType(pageNumber, pageSize, type);
   }
 
+  @ApiQuery({ name: 'pageNumber', example: 0 })
+  @ApiQuery({ name: 'pageSize', example: 10 })
+  @ApiQuery({ name: 'type', enum: WatchableType, required: false })
+  @ApiOkResponse({ 
+    type: Watchable, 
+    isArray: true,
+    description: 'Retrieve all latest Watchables by Type with pagination and sorting support.' 
+  })
   @Get('latest')
   findLatest(@Query('pageNumber') pageNumber: number = 0, 
              @Query('pageSize') pageSize: number = 10,
@@ -20,6 +39,14 @@ export class FilterController {
     return this.filterService.findLatest(pageNumber, pageSize, type);
   }
 
+  @ApiQuery({ name: 'pageNumber', example: 0 })
+  @ApiQuery({ name: 'pageSize', example: 10 })
+  @ApiQuery({ name: 'type', enum: WatchableType, required: false })
+  @ApiOkResponse({ 
+    type: Watchable, 
+    isArray: true,
+    description: 'Retrieve all popular Watchables by Type with pagination and sorting support.' 
+  })
   @Get('popular')
   findPopular(@Query('pageNumber') pageNumber: number = 0, 
               @Query('pageSize') pageSize: number = 10,
@@ -27,6 +54,14 @@ export class FilterController {
     return this.filterService.findPopular(pageNumber, pageSize, type);
   }
 
+  @ApiQuery({ name: 'pageNumber', example: 0 })
+  @ApiQuery({ name: 'pageSize', example: 10 })
+  @ApiQuery({ name: 'type', enum: WatchableType, required: false })
+  @ApiOkResponse({ 
+    type: Watchable, 
+    isArray: true,
+    description: 'Retrieve all upcoming Watchables by Type with pagination and sorting support.' 
+  })
   @Get('upcoming')
   findUpcoming(@Query('pageNumber') pageNumber: number = 0, 
                @Query('pageSize') pageSize: number = 10,
