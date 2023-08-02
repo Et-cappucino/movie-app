0825925e5ee6380c0293be136d74ac9d2efa2336
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-08-03 00:43:22 +0400

    Add exception filter, update main with exception filter

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './exception/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
     whitelist: true,
