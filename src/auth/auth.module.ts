626bf5cf42e7ce95aca43bcc3906e12eedce1075
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-08-04 02:04:31 +0400

    Add Access and Refresh token strategies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { AccessTokenStrategy, RefreshTokenStrategy } from './strategies';
 
 @Module({
   imports: [],
-  providers: [AuthService],
+  providers: [
+    AuthService, 
+    AccessTokenStrategy,
+    RefreshTokenStrategy
+  ],
   controllers: [AuthController]
 })
 export class AuthModule {}
