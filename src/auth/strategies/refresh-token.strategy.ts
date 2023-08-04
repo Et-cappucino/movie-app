b4cf9362ad5094bbe97a4ffc8f638edd5c9ec40a
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-08-05 00:21:12 +0400

    Update strategies with types and logic

--- src/auth/strategies/refresh-token.strategy.ts
+++ src/auth/strategies/refresh-token.strategy.ts
@@ -1,7 +1,9 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
+import { Request } from "express";
 import { ExtractJwt, Strategy } from "passport-jwt";
+import { JwtPayload } from "../types";
 
 @Injectable()
 export class RefreshTokenStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
@@ -13,7 +15,16 @@ export class RefreshTokenStrategy extends PassportStrategy(Strategy, 'jwt-refres
         })
     }
 
-    validate(payload: any) {
-        return payload;
+    validate(req: Request, payload: JwtPayload) {
+        const refreshToken = req?.get('authorization')
+                                ?.replace('Bearer', '')
+                                .trim();
+        
+        if (!refreshToken) throw new ForbiddenException('Refresh token malformed');
+
+        return {
+            ...payload,
+            refreshToken
+        }
     }
 } 
