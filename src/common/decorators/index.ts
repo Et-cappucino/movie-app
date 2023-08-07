0257b147aabf660882ccce15b9928106cd2efb3f
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-08-07 23:47:25 +0400

    Add custom Get current user email decorator, update userSearch with it

--- src/common/decorators/index.ts
+++ src/common/decorators/index.ts
@@ -1,3 +1,4 @@
 export * from './get-current-user-id.decorator';
+export * from './get-current-user-email.decorator';
 export * from './get-current-user-refresh-token.decorator';
 export * from './public.decorator';
