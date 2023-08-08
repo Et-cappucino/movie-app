db4e4fab001789ee086fee9cdc096400fa045374
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-08-08 21:56:23 +0400

    Update watchlist, favorites, and favorit genres controllers with getting userId from jwt token

--- src/favorite-genres/favorite-genres.controller.ts
+++ src/favorite-genres/favorite-genres.controller.ts
@@ -4,6 +4,7 @@ import { FavoriteGenresService } from './favorite-genres.service';
 import { UpdateFavoriteGenresDto } from './dto/favorite-genres.dto';
 import { GenreEnum } from 'src/watchable/enums';
 import { Genre } from 'src/watchable/entities';
+import { GetCurrentUserId } from 'src/common/decorators';
 
 @ApiTags('Favorite-Genres-Controller')
 @ApiBearerAuth()
@@ -18,16 +19,16 @@ export class FavoriteGenresController {
   })
   @ApiUnauthorizedResponse({ description: 'Unauthorized to get the favorite genre list of the User.' })
   @ApiNotFoundResponse({ description: 'Profile with provided id could not be found.' })
-  @Get(':profileId')
-  getProfileFavoriteGenres(@Param('profileId') profileId: number) {
+  @Get()
+  getProfileFavoriteGenres(@GetCurrentUserId() profileId: number) {
     return this.favoriteGenresService.getProfileFavoriteGenres(profileId);
   }
   
   @ApiOkResponse({ description: 'Genre has been successfully added to a favorite genre list.' })
   @ApiUnauthorizedResponse({ description: 'Unauthorized to add genre to a favorite genre list.' })
   @ApiNotFoundResponse({ description: 'Profile with provided id could not be found.' })
-  @Put(':profileId')
-  addToFavoriteGenres(@Param('profileId') profileId: number,
+  @Put()
+  addToFavoriteGenres(@GetCurrentUserId() profileId: number,
                       @Body() updateFavoriteGenresDto: UpdateFavoriteGenresDto) {
     this.favoriteGenresService.addToFavoriteGenres(profileId, updateFavoriteGenresDto);
   }
@@ -36,8 +37,8 @@ export class FavoriteGenresController {
   @ApiOkResponse({ description: 'Genre has been successfully removed from a favorite genre list.' })
   @ApiUnauthorizedResponse({ description: 'Unauthorized to remove genre from a favorite genre list.' })
   @ApiNotFoundResponse({ description: 'Profile with provided id could not be found.' })
-  @Delete(':profileId')
-  removeFromFavoriteGenres(@Param('profileId') profileId: number,
+  @Delete()
+  removeFromFavoriteGenres(@GetCurrentUserId() profileId: number,
                            @Query('genre') genre: GenreEnum) {
     this.favoriteGenresService.removeFromFavoriteGenres(profileId, genre);
   }
