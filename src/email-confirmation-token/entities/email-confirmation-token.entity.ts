415ff899016694840863d704e666b427381a4cc4
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-08-11 00:08:20 +0400

    Add email confirmation token entity

--- src/email-confirmation-token/entities/email-confirmation-token.entity.ts
+++ src/email-confirmation-token/entities/email-confirmation-token.entity.ts
@@ -1,4 +1,26 @@
-import { Entity } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity()
-export class EmailConfirmationToken {}
+export class EmailConfirmationToken {
+
+    @ApiProperty()
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @ApiProperty()
+    @Column()
+    token: string;
+
+    @ApiProperty()
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt: Date;
+
+    @ApiProperty()
+    @Column({ name: 'expires_at', type: 'datetime' })
+    expiresAt: Date;
+
+    @ApiProperty()
+    @Column({ name: 'confirmed_at', type: 'datetime', nullable: true })
+    confirmedAt: Date;
+}
