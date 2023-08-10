50ee9a5a5c2cda0dfb76476e4e49a284e2ba1802
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-08-10 18:14:17 +0400

    Add custom mail options, update MailSender service with mail sending logic

--- src/mail-sender/mail-sender.service.ts
+++ src/mail-sender/mail-sender.service.ts
@@ -1,4 +1,25 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MailOptions } from './types/mail-options.type';
 
 @Injectable()
-export class MailSenderService {}
+export class MailSenderService {
+    constructor(
+        private readonly configService: ConfigService,
+        private readonly mailSenderService: MailerService
+    ) {}
+    
+    async sendConfirmationMail(email: string) {
+        const options: MailOptions = {
+            to: email,
+            subject: 'Confirm your email to activate your profile!',
+            from: this.configService.getOrThrow<string>('MAIL_USERNAME'),
+            template: 'confirmation-email',
+            context: { 
+                link:'random_link_for_confirmation' 
+            }
+        }
+        await this.mailSenderService.sendMail(options);
+    }
+}
