e86b1b23d16b471930e81bc78a0afd8eefdd6c23
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-08-10 18:11:10 +0400

    Update MailSender module with template configs, add custom email template in views

--- src/mail-sender/mail-sender.module.ts
+++ src/mail-sender/mail-sender.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigService } from '@nestjs/config';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { join } from 'path';
 import { MailSenderService } from './mail-sender.service';
 
 @Module({
@@ -13,8 +15,12 @@ import { MailSenderService } from './mail-sender.service';
             user: configService.getOrThrow<string>('MAIL_USERNAME'),
             pass: configService.getOrThrow<string>('MAIL_PASSWORD')
           }
+        },
+        template: {
+          dir: join(__dirname, '../../views'),
+          adapter: new HandlebarsAdapter()
         }
-    }),
+      }),
     inject: [ConfigService],
   })],
   providers: [MailSenderService],
