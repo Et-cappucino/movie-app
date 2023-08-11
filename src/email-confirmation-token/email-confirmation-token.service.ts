c6106eb6fc9198e197530c42906b6ddf47ba8dea
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-08-11 22:40:35 +0400

    Update Email confiramtion token service with getting expiration minutes by configService

--- src/email-confirmation-token/email-confirmation-token.service.ts
+++ src/email-confirmation-token/email-confirmation-token.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { OnEvent } from '@nestjs/event-emitter';
+import { ConfigService } from '@nestjs/config';
 import { Repository } from 'typeorm';
 import { EmailConfirmationToken } from './entities/email-confirmation-token.entity';
 
@@ -8,7 +9,8 @@ import { EmailConfirmationToken } from './entities/email-confirmation-token.enti
 export class EmailConfirmationTokenService {
   constructor(
     @InjectRepository(EmailConfirmationToken)
-    private readonly emailConfirmationTokenRepository: Repository<EmailConfirmationToken>
+    private readonly emailConfirmationTokenRepository: Repository<EmailConfirmationToken>,
+    private readonly configService: ConfigService
   ) {}
   
   confirm(token: string) {
@@ -17,10 +19,12 @@ export class EmailConfirmationTokenService {
 
   @OnEvent('user-created')
   generateToken() {
+    const minutes = this.configService.getOrThrow<number>('EXPIRATION_MINUTES')
+    
     const token = this.emailConfirmationTokenRepository.create({
       token: 'random-token-uuid',
       createdAt: new Date(),
-      expiresAt: new Date(this.getExpirationTime(15))
+      expiresAt: new Date(this.getExpirationTime(minutes))
     })
     
     return this.emailConfirmationTokenRepository.save(token);
