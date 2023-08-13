6824f2f389e4ceee09866b9e35e02fb9aee13e36
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-08-13 13:41:19 +0400

    Update Email conf token controller with custom flters

--- src/email-confirmation-token/email-confirmation-token.controller.ts
+++ src/email-confirmation-token/email-confirmation-token.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Get, Query, Render } from '@nestjs/common';
+import { Controller, Get, Query, Render, UseFilters } from '@nestjs/common';
 import { EmailConfirmationTokenService } from './email-confirmation-token.service';
 import { Public } from 'src/common/decorators';
+import { ConfirmationTokenExpiredExceptionFilter, EmailAlreadyConfirmedFilter } from 'src/exception/filters';
 
 @Controller('confirm')
+@UseFilters(EmailAlreadyConfirmedFilter, ConfirmationTokenExpiredExceptionFilter)
 export class EmailConfirmationTokenController {
   constructor(private readonly emailConfirmationTokenService: EmailConfirmationTokenService) {}
 
   @Public()
   @Render('email-confirmed.hbs')
   @Get()
-  confirm(@Query('token') token: string) {
-    this.emailConfirmationTokenService.confirm(token);
+  async confirm(@Query('token') token: string) {
+    await this.emailConfirmationTokenService.confirm(token);
   }
 }
